89149f908744fdb2e8ea87317f904de6537640e6
vcharalampidis <vasilisharalabidis@gmail.com>
2019-07-12 17:49:37 +0300

    added Test for  Message Tag length

--- CSharp8583/CSharp8583.Tests/ConstantValues.cs
+++ CSharp8583/CSharp8583.Tests/ConstantValues.cs
@@ -11,10 +11,15 @@ namespace CSharp8583.Tests
         public static string ASCIIMessageWithResField = "01000000000000C00002JI09100300000000011111105801041234020B22222222222030EPROPERTY TAXES040D1111111111111";
         public static string ASCIIMessageWithSecondaryBitmap = "01009038040008C0000000800000000000000000000044440000211042120529021000000001015JI091003000000000111111121212";
         public static string ASCIIMessageMissingF41 = "01001038040008C000000000000044440000211042120529021000000001015000000000111111";
+        public static string ASCIIMessageTagLengthThree = "01000000000000C000020000000500000000000111203901004000002009FR44548810300522215040010";
+        public static string ASCIIMessageTagLengthTwo = "01000000000000C0000200000005000000000001112035010400000209FR445488103052221504010";
 
         public static byte[] ASCIIBytes = Encoding.ASCII.GetBytes(ASCIIMessage);
         public static byte[] ASCIIBytesMissingF41 = Encoding.ASCII.GetBytes(ASCIIMessageMissingF41);
         public static byte[] ASCIIBytesWithSecondaryBitmap = Encoding.ASCII.GetBytes(ASCIIMessageWithSecondaryBitmap);
         public static byte[] ASCIIBytesWithResField = Encoding.ASCII.GetBytes(ASCIIMessageWithResField);
+        public static byte[] ASCIIBytesTagLengthTwo = Encoding.ASCII.GetBytes(ASCIIMessageTagLengthTwo);
+        public static byte[] ASCIIBytesTagLengthThree = Encoding.ASCII.GetBytes(ASCIIMessageTagLengthThree);
+
     }
 }
---
+++ CSharp8583/CSharp8583.Tests/Messages/AsciiMessageWTagLengthII.cs
@@ -0,0 +1,34 @@
+﻿using CSharp8583.Attributes;
+using CSharp8583.Common;
+using CSharp8583.Messages;
+
+namespace CSharp8583.Tests.Messages
+{
+    public class AsciiMessageWTagLengthII : BaseMessage
+    {
+
+        [IsoField(position: IsoFields.F41, maxLen: 8, lengthType: LengthType.FIXED, contentType: ContentType.N)]
+        public string Field41 { get; set; }
+
+        [IsoField(position: IsoFields.F42, maxLen: 15, lengthType: LengthType.FIXED, contentType: ContentType.N)]
+        public string Field42 { get; set; }
+
+        [IsoField(position: IsoFields.F63, maxLen: 999, lengthType: LengthType.LLLVAR, contentType: ContentType.ANS)]
+        public _Field63 Field63 { get; set; }
+    }
+
+    public class _Field63 : CustomField
+    {
+        [Tag(position: 1, tagName: "01", lenBytesLen: 2, lenDataType: DataType.ASCII, dataType: DataType.ASCII)]
+        public virtual string Tag01 { get; set; }
+
+        [Tag(position: 2, tagName: "02", lenBytesLen: 2, lenDataType: DataType.ASCII, dataType: DataType.ASCII)]
+        public virtual string Tag02 { get; set; }
+
+        [Tag(position: 3, tagName: "03", lenBytesLen: 2, lenDataType: DataType.ASCII, dataType: DataType.ASCII)]
+        public virtual string Tag03 { get; set; }
+
+        [Tag(position: 4, tagName: "04", lenBytesLen: 2, lenDataType: DataType.ASCII, dataType: DataType.ASCII)]
+        public virtual string Tag04 { get; set; }
+    }
+}
